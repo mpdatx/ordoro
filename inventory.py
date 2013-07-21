4b0f27c714bd43933cad5bf240ff4918be2f5b24
Matthew Daniels <matthew@liveoak360.com>
2013-07-21 07:14:29 -0500

    Add the ability to retry failed async api calls

--- inventory.py
+++ inventory.py
@@ -1,5 +1,5 @@
 from suds.client import Client
-from eventlet import GreenPool
+from eventlet import GreenPool, monkey_patch
 
 def get_api_products():
     """Return a sample list of products."""
@@ -27,8 +27,20 @@ def connect_magento_store(store_id):
     session = client.service.login(store['username'], store['api_key'])
     return client, session
 
-def update_inventory(store_id, products, async=False, max_connections=10):
-    """Update inventory for a list of products for the given magento store."""
+def update_inventory(store_id, products, async=False, max_connections=15, retry=True):
+    """Update inventory for a list of products for the given magento store.
+
+    Keyword arguments:
+    store_id -- The store_id passed to connect_magento_store
+    products -- A list of product dicts, each of whic must contain an entry for cart_prod_id and qty
+    async -- If True, make the API calls concurrently with eventlet
+    max_connections -- If calling asynchronously, the max number of concurrent connections
+    retry -- Should we retry failed async calls?  These often fail due to SQL deadlocks caused by calling
+        the Magento api concurrently.  Retrying the failed calls lets us make every update properly and
+        still gain the massive speedup from doing it concurrently.  Code that makes use of this functionality
+        should keep an eye on the retry_count (3rd return) as some stores may be too slow to handle even 2
+        concurrent connections. 
+    """
     client, session = connect_magento_store(store_id)
     products_updated = []
     products_errored = []
@@ -37,7 +49,7 @@ def update_inventory(store_id, products, async=False, max_connections=10):
         try:
             return client.service.catalogInventoryStockItemUpdate(session, product_id, {'qty':qty})
         except:
-           return 0
+            return 0
 
     def callback(thread, id):
         result = thread.wait()
@@ -48,20 +60,31 @@ def update_inventory(store_id, products, async=False, max_connections=10):
 
     if async:
         pool = GreenPool(max_connections)
+        monkey_patch()
 
     for p in products:
         if async:
-            pool.spawn(api_call, p['product_id'], p['qty']).link(callback, p['product_id'])
+            pool.spawn(api_call, p['cart_prod_id'], p['qty']).link(callback, p['cart_prod_id'])
         else:
-            if api_call(p['product_id'], p['qty']) == 1:
-                products_updated.append(p['product_id'])
+            if api_call(p['cart_prod_id'], p['qty']) == 1:
+                products_updated.append(p['cart_prod_id'])
             else:
-                products_errored.append(p['product_id'])
+                products_errored.append(p['cart_prod_id'])
 
     if async:
         pool.waitall()
 
-    return products_updated, products_errored
+    retry_count = 0
+    if async and retry and len(products_errored) > 0:
+        while (len(products_errored) > 0) and (retry_count < len(products)*2): # @todo tweak the retry count
+            for p in products:
+                if p['cart_prod_id'] in products_errored:
+                    products_errored.pop( products_errored.index(p['cart_prod_id']))
+                    retry_count += 1
+                    pool.spawn(api_call, p['cart_prod_id'], p['qty']).link(callback, p['cart_prod_id'])
+            pool.waitall()
+
+    return products_updated, products_errored, retry_count
 
 if __name__ == '__main__':
     updated, errored = update_inventory('demo', [{'product_id':161, 'qty':995}, {'product_id':1611111, 'qty':995}])
