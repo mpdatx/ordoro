8c155edeb0f4848f2d5593033d7b733a47ae90de
Matthew Daniels <matthew@liveoak360.com>
2013-07-21 07:13:59 -0500

    Properly escape the skus in get_api_products

--- inventory.py
+++ inventory.py
@@ -4,9 +4,9 @@ from eventlet import GreenPool
 def get_api_products():
     """Return a sample list of products."""
     return [
-        {'sku': n2610, 'cart_prod_id': 16, 'name': 'Nokia 2610 Phone', 'qty': 996.0000}, 
-        {'sku': bb8100, 'cart_prod_id': 17, 'name': 'BlackBerry 8100 Pearl', 'qty': 797.0000}, 
-        {'sku': sw810i, 'cart_prod_id': 18, 'name': 'Sony Ericsson W810i', 'qty': 989.0000}
+        {'sku': 'n2610', 'cart_prod_id': 16, 'name': 'Nokia 2610 Phone', 'qty': 996.0000}, 
+        {'sku': 'bb8100', 'cart_prod_id': 17, 'name': 'BlackBerry 8100 Pearl', 'qty': 797.0000}, 
+        {'sku': 'sw810i', 'cart_prod_id': 18, 'name': 'Sony Ericsson W810i', 'qty': 989.0000}
     ]
 
 def connect_magento_store(store_id):
